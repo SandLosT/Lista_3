1c5e462f1599a85b6619dfbd9042769b93a9bab7
SandLosT <142359558+SandLosT@users.noreply.github.com>
2023-11-30 13:02:51 -0300

    Questão 3.c

--- 3.c
+++ 3.c
@@ -2,23 +2,24 @@
 #include <string.h>
 #include <ctype.h>
 
-
-int validadorplaca(char *numplaca) 
+// Função para validar a placa do veículo
+int validarPlaca(char *numeroPlaca) 
 {
-    int comprimento = strlen(numplaca);
+    int comprimento = strlen(numeroPlaca);
 
-    if ((comprimento == 8||comprimento == 7) &&
-        (isalpha(numplaca[0]) && isalpha(numplaca[1]) && isalpha(numplaca[2]) &&
-         (isdigit(numplaca[comprimento -1]) || isalpha(numplaca[3]) &&
-          isdigit(numplaca[comprimento -1])))) {
+    if ((comprimento == 8 || comprimento == 7) &&
+        (isalpha(numeroPlaca[0]) && isalpha(numeroPlaca[1]) && isalpha(numeroPlaca[2]) &&
+         (isdigit(numeroPlaca[comprimento - 1]) || isalpha(numeroPlaca[3]) &&
+          isdigit(numeroPlaca[comprimento - 1])))) {
         return 1;
     }
     return 0;
 }
 
+// Função para validar o dia da semana
 int validarDiaSemana(char *nomeDia) 
 {
-    const char *DiasSemana[] = 
+    const char *diasSemana[] = 
     {"SEGUNDA-FEIRA",
      "TERCA-FEIRA", 
      "QUARTA-FEIRA", 
@@ -28,30 +29,29 @@ int validarDiaSemana(char *nomeDia)
      "DOMINGO"};
 
     for (int i = 0; i < 7; i++) {
-        if (strcmp(nomeDia, DiasSemana[i]) == 0) {
+        if (strcmp(nomeDia, diasSemana[i]) == 0) {
             return 1;
         }
     }
     return 0;
 }
 
+// Função principal
 int main(void) 
 {
-    char placaVeiculo[10], diaSemana[20];
+    char numeroPlacaVeiculo[10], diaSemana[20];
 
-    scanf("%s", placaVeiculo);
+    scanf("%s", numeroPlacaVeiculo);
 
-    for (int i = 0; i < strlen(placaVeiculo); i++)
-     {
-        placaVeiculo[i] = toupper(placaVeiculo[i]);
+    for (int i = 0; i < strlen(numeroPlacaVeiculo); i++) {
+        numeroPlacaVeiculo[i] = toupper(numeroPlacaVeiculo[i]);
     }
 
     scanf("%s", diaSemana);
 
-    int ultimoDigitoPlaca = placaVeiculo[strlen(placaVeiculo) - 1] - '0';
+    int ultimoDigitoPlaca = numeroPlacaVeiculo[strlen(numeroPlacaVeiculo) - 1] - '0';
 
-    if (!validadorplaca(placaVeiculo))
-     {
+    if (!validarPlaca(numeroPlacaVeiculo)) {
         printf("Placa invalida\n");
         if (!validarDiaSemana(diaSemana)) {
             printf("Dia da semana invalido\n");
@@ -59,31 +59,27 @@ int main(void)
         return 0;
     }
 
-    if (!validarDiaSemana(diaSemana))
-     {
+    if (!validarDiaSemana(diaSemana)) {
         printf("Dia da semana invalido\n");
         return 0;
     }
 
-    if (strcmp(diaSemana, "SABADO") == 0 || strcmp(diaSemana, "DOMINGO") == 0) 
-    {
+    if (strcmp(diaSemana, "SABADO") == 0 || strcmp(diaSemana, "DOMINGO") == 0) {
         printf("Nao ha proibicao no fim de semana\n");
     } else {
-        for (int i = 0; i < strlen(diaSemana); i++) 
-        {
+        for (int i = 0; i < strlen(diaSemana); i++) {
             diaSemana[i] = tolower(diaSemana[i]);
         }
 
-        const char *DiasSemanaLowerCase[] = {"segunda-feira", "terca-feira", "quarta-feira", "quinta-feira",
+        const char *diasSemanaLowerCase[] = {"segunda-feira", "terca-feira", "quarta-feira", "quinta-feira",
                                               "sexta-feira"};
 
-        for (int i = 0; i < 5; i++) 
-        {
-            if (strcmp(diaSemana, DiasSemanaLowerCase[i]) == 0) {
+        for (int i = 0; i < 5; i++) {
+            if (strcmp(diaSemana, diasSemanaLowerCase[i]) == 0) {
                 if ((ultimoDigitoPlaca == i * 2 || ultimoDigitoPlaca == i * 2 + 1)) {
-                    printf("%s nao pode circular %s\n", placaVeiculo, diaSemana);
+                    printf("%s nao pode circular %s\n", numeroPlacaVeiculo, diaSemana);
                 } else {
-                    printf("%s pode circular %s\n", placaVeiculo, diaSemana);
+                    printf("%s pode circular %s\n", numeroPlacaVeiculo, diaSemana);
                 }
                 break;
             }
